00162746fc79806da209c84ac0b73b95b492b409
Pravins9665 <92073366+Pravins9665@users.noreply.github.com>
2021-11-01 15:11:30 +0530

    Update System2.java

--- systemclass/System2.java
+++ systemclass/System2.java
@@ -3,19 +3,19 @@ package systemclass;
 import java.util.Scanner;
 
 class maths {
-	int a, b;
+	int a1, b1;
 
 	void input() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter first value ");
-		a = sc.nextInt();
+		a1 = sc.nextInt();
 		System.out.println("enter second value ");
-		b = sc.nextInt();
+		b1 = sc.nextInt();
 		sc.close();
 	}
 
 	void output() {
-		int sum = a + b;
+		int sum = a1 + b1;
 		System.out.print("addition of two numbers is : " + sum);
 	}
 }
